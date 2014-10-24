5716a54bf15d2a4c42b2bd71957a76bf5e1e0cbf
Colin <colinad70@gmail.com>
2014-10-24 15:10:51 -0700

    keys longer and detecting walls
    made it so can use keys cant turn around when runs into food get longer gut food doesn't go away and in console tells you when you go off the screen

---
+++ public_html/js/snake.js
@@ -0,0 +1,152 @@
+var snake;
+var snakeLength;
+var snakeSize;
+var snakeDirection;
+
+var food;
+var foodSize;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+gameInitialize();
+snakeInitialize();
+foodInitialize();
+setInterval(gameLoop, 1000/15);
+
+
+function gameInitialize() {
+    var canvas = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
+    
+    document.addEventListener("keydown", keyboardHandler);
+}
+
+function gameLoop() {
+    gameDraw();
+    snakeUpdate();
+    snakeDraw();
+    foodDraw();
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(0, 0, 0)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    snake = [];
+    snakeLength = 3;
+    snakeSize = 30;
+    snakeDirection = "down";
+    
+    for(var index = snakeLength - 1; index >= 0; index--) {
+        snake.push( {
+            x: index,
+            y: 0
+            });
+    }
+}
+
+function snakeDraw() {
+    for(var index = 0; index < snake.length; index++) {
+        context.fillStyle = "orange";
+        context.fillRect(snake[index].x * snakeSize, snake[index].y * snakeSize, snakeSize, snakeSize);
+    }
+}
+
+function snakeUpdate() {
+    var snakeHeadX = snake[0].x;
+    var snakeHeadY = snake[0].y;
+    
+    if(snakeDirection == "down") {
+        snakeHeadY++;
+    }
+    else if(snakeDirection == "up"){
+        snakeHeadY--;
+    }
+    else if(snakeDirection == "right"){
+        snakeHeadX++;
+    }
+    else if(snakeDirection == "left"){
+        snakeHeadX--;
+    }
+
+    checkFoodCollisions(snakeHeadX, snakeHeadY);
+    checkWallCollisions(snakeHeadX, snakeHeadY);
+    
+    var snakeTail = snake.pop();
+    snakeTail.x = snakeHeadX;
+    snakeTail.y = snakeHeadY;
+    snake.unshift(snakeTail);
+}
+
+function foodInitialize() {
+    foodSize = 30;
+    
+    food = {
+        x: 0,
+        y: 0
+    };
+    setFoodPosition();
+}
+
+function foodDraw() {
+    context.fillStyle = "lime";
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, foodSize, foodSize);
+}
+
+function setFoodPosition() {
+    var randomX = Math.floor(Math.random() * screenWidth);
+    var randomY = Math.floor(Math.random() * screenHeight);
+    
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
+}
+
+function keyboardHandler(event) {
+    console.log(event);
+    
+    if(event.keyCode == "39" && snakeDirection != "left") {
+        snakeDirection = "right";
+    }
+    
+    else if(event.keyCode == "37" && snakeDirection != "right") {
+        snakeDirection = "left";
+    }
+    
+    else if(event.keyCode == "38" && snakeDirection != "down") {
+        snakeDirection = "up";
+    }
+    
+    else if(event.keyCode == "40" && snakeDirection != "up") {
+        snakeDirection = "down";
+    }
+}
+
+function checkFoodCollisions(snakeHeadX, snakeHeadY) {
+    if(snakeHeadX == food.x && snakeHeadY == food.y) {
+        snake.push({
+           x: 0,
+           y: 0
+        });
+        snakeLength++;
+    }
+}
+
+function checkWallCollisions(snakeHeadX, snakeHeadY) {
+    if(snakeHeadX * snakeSize >= screenWidth || snakeHeadX * snakeSize < 0) {
+        console.log("wall collision")
+    }
+    
+    else if(snakeHeadY * snakeSize >= screenHeight || snakeHeadY * snakeSize < 0) {
+        console.log("wall collision")
+    }
+}
